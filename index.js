a0b2f0e0eb105fa9f320736a58998a1ae9e263ab
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-24 07:25:47 +0900

    Completed Assignment

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-
-// function superbowlWin() {
-//   for (let item of record) {
-//     if (item.result === "W") {
-//       let winResult = item.year
-//       return winResult
-//     }
-//     if (item.result.find())
-//   }
-// }
-
-// function superbowlWin(item) {
-//     let winResult = item.find( ({result}) => result === "W" )
-//     if (winResult.result === "W") {
-//       return winResult.year
-//     } 
-// }
-
-function superbowlWin(record) {
-  for (let item of record) {
-    if (item.result === "W") {
-      let winResult = item.year
-      return winResult
-    }
+function superbowlWin(item) {
+  let win = item.find(item => item.result === "W")
+  try{
+  if (win.result == "W") {
+  return win.year
+}
+  } catch(error) {
+    return undefined;
   }
 }
