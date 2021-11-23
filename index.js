d9ad06719d47f451dbf10476dcb0bd841641f649
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-24 05:05:36 +0900

    Completed, but I can't find solution using .find()

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+// function superbowlWin() {
+//   for (let item of record) {
+//     if (item.result === "W") {
+//       let winResult = item.year
+//       return winResult
+//     }
+//     if (item.result.find())
+//   }
+// }
+
+// function superbowlWin(item) {
+//     let winResult = item.find( ({result}) => result === "W" )
+//     if (winResult.result === "W") {
+//       return winResult.year
+//     } 
+// }
+
+function superbowlWin(record) {
+  for (let item of record) {
+    if (item.result === "W") {
+      let winResult = item.year
+      return winResult
+    }
+  }
+}
