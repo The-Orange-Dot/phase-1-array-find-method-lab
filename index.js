411ab70901c3698376a1f9ff855ef0dffcc6c08f
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-24 11:04:57 +0900

    Completed Assigment (Fixed)

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 function superbowlWin(item) {
-  let win = item.find(item => item.result === "W")
-  try{
-  if (win.result == "W") {
-  return win.year
-}
-  } catch(error) {
+  let win = item.find((item) => item.result === "W");
+  if (win && win.result == "W") {
+    return win.year;
+  } else {
     return undefined;
   }
 }
